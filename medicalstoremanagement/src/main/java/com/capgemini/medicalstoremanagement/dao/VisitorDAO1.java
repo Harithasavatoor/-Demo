00bc67bc817c4d9a75ce2179b5cfd225fd9dd508
Harithasavatoor <harithasavatoor@gmail.com>
2020-02-09 16:42:06 +0530

    visitor module

--- medicalstoremanagement/src/main/java/com/capgemini/medicalstoremanagement/dao/VisitorDAO1.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.medicalstoremanagement.dao;
-
-import com.capgemini.medicalstoremanagement.dto.VisitorMedicineInfo;
-
-public interface VisitorDAO1 {
-	static VisitorMedicineInfo visitormedinfo=new VisitorMedicineInfo();
-	void medicineInfo() throws InstantiationException, IllegalAccessException, ClassNotFoundException;
-
-}
